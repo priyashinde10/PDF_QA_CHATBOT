af1e35f35640f0b667966e662b7c0b7c6dfdde36
Priya Shinde <priyashinde100304@gmail.com>
2026-08-06 19:40:33 +0530

    Update Gemini model

--- utils/qa.py
+++ utils/qa.py
@@ -10,7 +10,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-flash-lite",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0
 )
